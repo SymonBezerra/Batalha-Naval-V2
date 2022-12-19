442d2bf049b7e43f136b8924c3b20a6a987e86c7
SymonBezerra <simaobezerratranslate@gmail.com>
2022-12-19 19:37:47 -0300

    first game loop

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 import pygame
+screen = pygame.display.set_mode([1080, 720])
+# clock = pygame.time.Clock()
 
 if __name__ == "__main__":
     pygame.init()
 
-    screen = pygame.display.set_mode([1080, 720])
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        
+        screen.fill((255,255,255))
+
+        pygame.draw.circle(screen, (0,0,0), (250, 250), 50)
+
+        pygame.display.flip()
+    
+    pygame.quit()
