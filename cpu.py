94dd29a2fe73d12edc6e052a99b60e1d0b6f5a22
SymonBezerra <107515096+SymonBezerra@users.noreply.github.com>
2022-12-23 15:11:47 -0300

    new randomshot impl

--- cpu.py
+++ cpu.py
@@ -4,6 +4,12 @@ import pygame
 class CPU(Player):
     def __init__ (self, size: int, name: str):
         super().__init__(size, name)
+        self.size = size
+        self.name = name
+        self.remaining_shots = []
+        for i in range(self.size):
+            for j in range(self.size):
+                self.remaining_shots.append((i,j))
 
     def randomshot (self):
-        return (randint(0,8), randint(0,8))
+        return self.remaining_shots.pop(randint(0, len(self.remaining_shots) - 1))
